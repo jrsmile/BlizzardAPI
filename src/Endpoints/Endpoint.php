9ebf9b4dea48e4d0713d837050132c8b171293fa
Jared Falkenhagen <jared@falkenhagen.de>
2018-11-08 21:17:14 +0100

    Added failed request handling and cleaned up Endpoint class
    - A failed request now will answer with an special exception which contains the response object.
    - In addition the retry loop moved into class variables so the $counter parameter is hidden.
    - The profiling parameter are now without editor may missing variable notices.

--- src/Endpoints/Endpoint.php
+++ src/Endpoints/Endpoint.php
@@ -4,6 +4,7 @@ namespace BlizzardApiService\Endpoints;
 
 
 use BlizzardApiService\Context\ApiContext;
+use BlizzardApiService\Exceptions\ApiException;
 use BlizzardApiService\Settings\ApiUrls;
 use GuzzleHttp\Exception\ServerException;
 
@@ -14,6 +15,7 @@ class Endpoint
     protected $language      = false;
     protected $namespace     = false;
     protected $parameters    = [];
+    protected $retryCounter  = 0;
 
 
     protected $wholeUrl    = false;
@@ -26,7 +28,11 @@ class Endpoint
         $this->apiContext = $blizzardApiContext;
     }
 
-    protected function sendRequest($counter = 1){
+    /**
+     * @return mixed
+     * @throws ApiException
+     */
+    protected function sendRequest(){
         if( $this->wholeUrl === false) {
             if($this->requestUrl != false){
                 $url = ApiUrls::getBaseUrl($this->apiContext->getRegion()) . $this->requestUrl;
@@ -37,41 +43,54 @@ class Endpoint
         }else{
             $url = $this->wholeUrl;
         }
+
         if($this->namespace !== false){
             $this->parameters['namespace'] = $this->namespace;
         }
         $this->parameters['locale']       = $this->apiContext->getLocale();
         $this->parameters['access_token'] = $this->apiContext->getAccessToken();
+
         if(strpos($url, '?') !== false){
             $splitter = '&';
         }else{
             $splitter = '?';
         }
-        $finalUrl   = $url . $splitter . urldecode(http_build_query($this->parameters));
+
+        $finalUrl         = $url . $splitter . urldecode(http_build_query($this->parameters));
         $this->parameters = [];
-        if($this->apiContext->isProfiling()){
+        $profilingActive  = $this->apiContext->isProfiling();
+        $measureStart     = false;
+        if($profilingActive){
             $measureStart = microtime(true);
         }
 
         try {
             $response = $this->apiContext->sendRequest($finalUrl);
         }catch (ServerException $exception){
-            if($counter <= $this->apiContext->getRetryLimit()){
+            if($this->retryCounter <= $this->apiContext->getRetryLimit()){
                 sleep($this->apiContext->getRetrySleepTime());
-                return $this->sendRequest(++$counter);
+                $this->retryCounter++;
+                return $this->sendRequest();
             }
-            throw $exception;
+            throw (new ApiException(
+                'Error connecting to API: [' . $exception->getCode() . '] ', 0, $exception
+            ));
         }
+        $this->retryCounter = 0;
 
-        if($this->apiContext->isProfiling()){
+        if($profilingActive){
             $requestTime = microtime(true) - $measureStart;
             $this->apiContext->addMeasurement(get_class(), $requestTime);
         }
 
         if($response->getStatusCode() !== 200){
-            throw new \Exception(
+            $responseBody = @json_decode((string) $response->getBody());
+            if(json_last_error() == JSON_ERROR_NONE){
+                $responseBody = false;
+            }
+            throw (new ApiException(
                 'Error connecting to API: [' . $response->getStatusCode() . '] ' . $response->getReasonPhrase()
-            );
+            ))->setApiResponse($responseBody);
         }
         return json_decode((string) $response->getBody());
     }
