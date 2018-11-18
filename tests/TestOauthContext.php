6e4316f6c5f3012c5052af313dc2818cf01e2497
Jared Falkenhagen <jared@falkenhagen.de>
2018-11-18 02:19:28 +0100

    Corrected Test

--- tests/TestOauthContext.php
+++ tests/TestOauthContext.php
@@ -16,7 +16,7 @@ class TestOauthContext extends BlizzardOauthContext
         return 'de_DE';
     }
 
-    public function setAccessToken($key)
+    public function setAccessToken(string $key)
     {
         $this->accessToken = $key;
     }
